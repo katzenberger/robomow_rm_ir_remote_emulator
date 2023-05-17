53ddfd71673cd4e6643b7afc91751dfd8e502bd7
Péter Katzenberger <p.katzenberger@gmail.com>
2023-05-17 10:18:04 +0200

    Refactored code. Extracted signal level toggling logic into separated functions.

--- robomow_rm_ir_remote_simulator.ino
+++ robomow_rm_ir_remote_simulator.ino
@@ -5,34 +5,43 @@
 #define BIT_LEN_H 900
 #define BIT_LEN_L 670
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(IR_OUTPUT, OUTPUT);
-  Serial.begin(115200);
+static uint8_t incomingByte = 0;
+static bool irState = false;
+
+void changeSignalEdge() {
+    digitalWrite(IR_OUTPUT, irState);
+    irState = !irState;
 }
 
-void loop() 
-{
+void sendDataBits(uint32_t data, uint8_t bits) {
+  for (uint8_t i = 0; i < bits; i++) {
+    if (data & ((uint32_t)1u << i)) {
+      delayMicroseconds(BIT_LEN_H);
+    } else {
+      delayMicroseconds(BIT_LEN_L);
+    }
+    changeSignalEdge();
+  }
+}
 
-  static uint8_t incomingByte = 0;
-  static bool irState = false;
-  // put your main code here, to run repeatedly:
-  // send data only when you receive data:
+void receiveSerialCommand() {
   while (Serial.available() > 0) {
     incomingByte = Serial.read();
     Serial.write(incomingByte);
   }
+}
+
+void setup() {
+  pinMode(IR_OUTPUT, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  receiveSerialCommand();
     
-  // preamble
-  for (uint8_t i = 0; i<1; i++) {
-    digitalWrite(IR_OUTPUT, irState);
-    irState = !irState;
-    delay(12);
-  }
-
   uint32_t header = 0x5A5;
-
   uint32_t body = 0xAAAAAA;
+
   if (incomingByte == '1')
     body = 0x5AAAAA;
   else if (incomingByte == '2')
@@ -46,36 +55,17 @@ void loop()
   else if (incomingByte == '6')
     body = 0xAAAAA5;
 
-  digitalWrite(IR_OUTPUT, irState);
-  irState = !irState;
-
-  for (uint8_t i = 0; i<12; i++) {
-    if (header & ((uint32_t)1u<<i))
-      delayMicroseconds(BIT_LEN_H);
-    else
-      delayMicroseconds(BIT_LEN_L);
-    digitalWrite(IR_OUTPUT, irState);
-    irState = !irState;
-  }
-
-  for (uint8_t i = 0; i<23; i++) {
-    if (body & ((uint32_t)1u<<i))
-      delayMicroseconds(BIT_LEN_H);
-    else
-      delayMicroseconds(BIT_LEN_L);
-    digitalWrite(IR_OUTPUT, irState);
-    irState = !irState;        
-  }
-
-
-  delay(13);
-  digitalWrite(IR_OUTPUT, irState);
-  irState = !irState;
+  // preamble
+  changeSignalEdge();
+  delay(12);
+  changeSignalEdge();
+  // header
+  sendDataBits(header, 12);
+  // body
+  sendDataBits(body, 23);
   // close
-  /*for (uint8_t i = 0; i<2; i++) {
-    digitalWrite(IR_OUTPUT, irState);
-    irState = !irState;
-    delay(12);
-  }*/
+  delay(13);
+  changeSignalEdge();
+
   delay(100);
 }
